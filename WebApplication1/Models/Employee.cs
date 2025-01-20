a4e7d508ce037502d81265bfd81b675ea0dda12d
Soufiane Cherkaoui <soufiane.cherkaoui01@gmail.com>
2025-01-20 01:52:05 +0000

    feat & fix

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmplyeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+
+    }
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08644e8eb482edce58160cc29dfc50e312fa2076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef832abb215f0b0158a7039234c0b0a248b82f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
